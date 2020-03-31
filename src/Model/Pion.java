12801332712cb063c2b1d67f426fc5c711d2c6ca
goldrunner68 <pilouweb2000@gmail>
2020-03-31 15:33:15 +0200

    les pions

--- src/Model/Pion.java
+++ src/Model/Pion.java
@@ -1,4 +1,13 @@
 package Model;
 
 public class Pion {
+
+
+    public boolean sonTour( boolean cmonTour ) {
+        return true;
+    }
+
+    public boolean ajoutPion( boolean possibleAjoutPion ) {
+        return true;
+    }
 }
