cd876b90ec142a7a2e7b1c8a76ec8fb712d0d26e
aboubakr bouzana <bouzana-aboubakr@hotmail.com>
2020-03-31 16:42:41 +0200

    bouba3

--- src/Model/Plateau.java
+++ src/Model/Plateau.java
@@ -1,4 +1,32 @@
 package Model;
 
+
 public class Plateau {
+
+    public static int[][]Case;
+
+    public Plateau() {
+
+        Case = new int[3][3];
+        for (int i = 0; i < 3; i++) {
+            for (int y = 0; y < 3; y++) {
+
+                Case[i][y] = 0;
+            }
+        }
+
+    }
+
+    public boolean TestCaseLigne(int i, int y) {
+        return true;
+    }
+
+    public boolean TestCaseCol(int i, int y) {
+        return true;
+    }
+
+    public boolean TestCaseDiagonal(int i, int y) {
+        return true;
+    }
 }
+
