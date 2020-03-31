e33db3f350534ab94e049ee6ce8edf24c5a11ad1
goldrunner68 <pilouweb2000@gmail>
2020-03-31 17:01:46 +0200

    remarque explicative

--- src/Model/Pion.java
+++ src/Model/Pion.java
@@ -2,15 +2,15 @@ package Model;
 
 public class Pion {
 
-
+// croix true , rond false
     public boolean croixOuRond( boolean croixOuRond ) {
         return croixOuRond;
     }
-
+// possible ajout pion(croix et/ou rond) true emplacement dispo , false  car emplacemnt non dispo
     public boolean possibleAjoutPion( boolean possibleAjoutPion ) {
         return possibleAjoutPion;
     }
-
+//verifie si les pion(croix et/ou rond) sont alinge
     public boolean pionAlinge( boolean pionAligne ) {
         return pionAligne ;
     }
