f9c801272bf245d31a4989803ea162dfc5f32a8f
CloudyoErzod <cloudyo.pro@gmail.com>
2020-04-02 14:06:16 +0200

    Commit Class Partie et PartieTest

--- src/Model/Partie.java
+++ src/Model/Partie.java
@@ -1,4 +1,42 @@
 package Model;
 
 public class Partie {
+    Plateau plateau;
+
+    public Partie(int taillePlateau) {
+        plateau = new Plateau(taillePlateau);
+    }
+
+    public void jouer() {
+        boolean joueurGagne = false;
+        boolean egalite = false;
+        int col, row;
+        do {
+            System.out.println(plateau);
+            System.out.println("Choisissez une case.");
+            System.out.println("Ligne (de 1 ŕ "+plateau.taillePlateau+") :");
+            row = Lire.Entier() - 1;
+            System.out.println("Colonne (de 1 ŕ "+plateau.taillePlateau+") :");
+            col = Lire.Entier() - 1;
+            if (plateau.jouer(row, col)) {
+                if (plateau.victoire('X')) {
+                    joueurGagne = true;
+                    System.out.println(plateau);
+                    System.out.println("Fin de partie.");
+                } else {
+                    if (plateau.victoire('O')) {
+                        System.out.println(plateau);
+                        System.out.println("Fin de partie.");
+                    }
+                }
+            }
+            if (plateau.fin() && joueurGagne == false) {
+                egalite = true;
+                System.out.println(plateau);
+                System.out.println("Fin de partie. Egalité.");
+            }
+        } while (joueurGagne == false  && egalite == false);
+    }
+
+
 }
--- test/Model/PartieTest.java
+++ test/Model/PartieTest.java
@@ -4,5 +4,14 @@ import org.junit.jupiter.api.Test;
 
 
 public class PartieTest {
+    @Test
+    public void partieTest() {
+        Partie maPartie = new Partie();
+    }
+
+    @Test
+    public void jouerTest(){
+        Partie maPartie = new Partie();
+    }
 
 }
