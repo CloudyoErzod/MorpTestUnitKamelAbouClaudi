f892577b15e24ec010c663f99cd9db3a9fbd36a2
CloudyoErzod <cloudyo.pro@gmail.com>
2020-04-02 11:45:19 +0200

    MAJ Joueur

--- src/Model/Joueur.java
+++ src/Model/Joueur.java
@@ -1,18 +1,39 @@
 package Model;
 
 public class Joueur {
+    /**
+     *
+     * il faut verifier si la colone, ligne ou diagonal sont
+     * rempli pour le joueur
+     *
+     */
     public boolean aPerdu(boolean bPerdu) {
         return bPerdu;
     }
-
+    /**
+     *
+     * il faut verifier si la colone, ligne ou diagonal sont
+     * rempli pour le joueur
+     *
+     */
     public boolean aGagne(boolean bGagne) {
         return bGagne;
     }
-
+    /**
+     *
+     * il faut verifier a qui est le tour par rapport
+     * aux class Pion et Plateau
+     *
+     */
     public boolean tour(boolean bTour) {
         return bTour;
     }
-
+    /**
+     *
+     * il faut verifier la position par rapport
+     * a la class Plateau
+     *
+     */
     public int position(int y, int x) {
         return y + x;
     }
