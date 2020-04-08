9e81a2f077ff4e0bd0567e9896cdba0dd1263807
CloudyoErzod <cloudyo.pro@gmail.com>
2020-04-08 18:22:03 +0200

    Dernier Commit 2

---
+++ test/model/PartieTest2.java
@@ -0,0 +1,44 @@
+package model;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.Scanner;
+import static org.junit.jupiter.api.Assertions.*;
+
+
+public class PartieTest2 {
+
+    Partie partie ;
+    Scanner scanner;
+
+    @BeforeEach
+    void setUp() throws FileNotFoundException { // appele a chaque test
+        //intialise mes variable
+        partie = new Partie(3);
+    }
+
+    @Test
+    void testJoueur1Gagne() throws FileNotFoundException {
+        File test = new File("src/test/resources/jouer1Gagne.txt");
+        scanner = new Scanner(test);
+        partie.scanner = scanner;
+        partie.jouer();
+        assertEquals('X', partie.plateau.typeDePionAligne);
+        assertFalse(partie.matchNul());
+        assertTrue(partie.victoire());
+        scanner.close();
+    }
+
+    @Test
+    void testMatchNull() throws FileNotFoundException {
+        File test = new File("src/test/resources/matchNull.txt");
+        scanner = new Scanner(test);
+        partie.scanner = scanner;
+        partie.jouer();
+        assertTrue(partie.matchNul());
+        assertFalse(partie.victoire());
+        scanner.close();
+    }
+}
